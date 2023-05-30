8ece82300075786bc1ac04fe388f51ce16148531
Amael Marquez <amael.marquez@irit.fr>
2023-05-30 15:59:24 +0200

    Clean up whitspace

--- Ponca/src/Common/Assert.h
+++ Ponca/src/Common/Assert.h
@@ -7,7 +7,7 @@
 #define PONCA_ASSERT(EXPR)         check(EXPR)
 #define PONCA_ASSERT_MSG(EXPR,MSG) checkf((EXPR), TEXT(MSG))
 
-#define PONCA_DEBUG_ERROR                 checkfSlow(false, TEXT(""))
-#define PONCA_DEBUG_ERROR_MSG(MSG)        checkfSlow(false, TEXT(MSG))
-#define PONCA_DEBUG_ASSERT(EXPR)          checkSlow(EXPR)
-#define PONCA_DEBUG_ASSERT_MSG(EXPR,MSG)  checkfSlow((EXPR), TEXT(MSG))
+#define PONCA_DEBUG_ERROR                checkfSlow(false, TEXT(""))
+#define PONCA_DEBUG_ERROR_MSG(MSG)       checkfSlow(false, TEXT(MSG))
+#define PONCA_DEBUG_ASSERT(EXPR)         checkSlow(EXPR)
+#define PONCA_DEBUG_ASSERT_MSG(EXPR,MSG) checkfSlow((EXPR), TEXT(MSG))
