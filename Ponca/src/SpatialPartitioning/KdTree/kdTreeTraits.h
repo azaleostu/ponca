96e00d39a87e94d07b638438aa366635441d0c46
Amael Marquez <amael.marquez@irit.fr>
2023-06-02 16:54:51 +0200

    Reduce copies when PointContainer and PointUserContainer are not the same type

--- Ponca/src/SpatialPartitioning/KdTree/kdTreeTraits.h
+++ Ponca/src/SpatialPartitioning/KdTree/kdTreeTraits.h
@@ -27,21 +27,73 @@ namespace internal
         return count;
 #endif
     }
+
+    /*!
+     * \brief Calculates bitfield sizes of a default kd-tree inner node at compile-time.
+     *
+     * \tparam Index The type used to index data points.
+     * \tparam DIM The dimension of the data points.
+     */
+    template <typename Index, int DIM>
+    struct KdTreeDefaultInnerNodeBitfieldInfo
+    {
+        using UIndex = typename std::make_unsigned<Index>::type;
+
+        static_assert(DIM >= 0, "Dim must be positive");
+
+        enum
+        {
+            /*!
+             * \brief The minimum bit width required to store the point dimension.
+             *
+             * Equal to the index of DIM's most significant bit (starting at 1), e.g.:
+             * With DIM = 4,
+             *             -------------
+             * DIM =    0b | 1 | 0 | 0 |
+             *             -------------
+             * Bit index =  #3  #2  #1
+             *
+             * The MSB has an index of 3, so we store the dimension on 3 bits.
+             */
+            DIM_BITS = sizeof(unsigned int)*8 - internal::clz((unsigned int)DIM),
+            UINDEX_BITS = sizeof(UIndex)*8,
+        };
+
+        // 1 bit is reserved for the leaf flag
+        static_assert(DIM_BITS < UINDEX_BITS - 1,
+            "Dim does not fit in the index bitfield of a default inner node");
+
+        enum
+        {
+            /*!
+             * \brief The number of remaining bits that can be used to store indices.
+             */
+            CHILD_ID_BITS = UINDEX_BITS - (DIM_BITS + 1),
+        };
+    };
 }
 
 template <typename Index, typename Scalar, int DIM>
 struct KdTreeDefaultInnerNode
 {
 private:
-    using UIndex = typename std::make_unsigned<Index>::type;
-
-    static_assert(DIM >= 0, "Dim must be positive");
+    using BitfieldInfo = internal::KdTreeDefaultInnerNodeBitfieldInfo<Index, DIM>;
+    using UIndex       = typename BitfieldInfo::UIndex;
 
 public:
     enum
     {
-        DIM_BITS = sizeof(unsigned int)*8 - internal::clz((unsigned int)DIM),
-        INDEX_BITS = sizeof(UIndex)*8 - (DIM_BITS + 1),
+        /*!
+         * \brief The number of bits used to store the point dimension.
+         *
+         * Points being in higher dimensions will result in less possible points being stored in the kd-tree.
+         */
+        DIM_BITS = BitfieldInfo::DIM_BITS,
+
+        /*!
+         * \brief The number of bits used to store point indices.
+         */
+        INDEX_BITS = BitfieldInfo::CHILD_ID_BITS,
     };
 
     Scalar split_value;
@@ -61,12 +113,9 @@ template <typename Index, typename Scalar, int DIM>
 struct KdTreeDefaultNode
 {
     using LeafSizeType = unsigned short;
+    using InnerType    = KdTreeDefaultInnerNode<Index, Scalar, DIM>;
+    using LeafType     = KdTreeDefaultLeafNode<Index, LeafSizeType>;
 
-private:
-    using InnerType = KdTreeDefaultInnerNode<Index, Scalar, DIM>;
-    using LeafType  = KdTreeDefaultLeafNode<Index, LeafSizeType>;
-
-public:
     union
     {
         InnerType inner;
@@ -77,6 +126,7 @@ public:
     void set_is_leaf(bool new_is_leaf) { inner.leaf = new_is_leaf; }
 };
 
+/// \todo Documentation
 template <typename _DataPoint>
 struct KdTreeDefaultTraits
 {
